919ffcd56b64447fa005cfb075e9f2ad99440d26
G-Njunge <g.njunge@alustudent.com>
2025-07-23 17:47:19 +0200

    Input validation 1

--- user_class.py
+++ user_class.py
@@ -10,24 +10,60 @@ class User:
         self.age = None
         self.prev_diagnosis= None
         self.child_details = None
+        self.parent_id = None
 
     def Reg_user(self):
         print("Holla! Please provide your account details for registration")
+        print("Loading...")
+        # Establishing a connection to the database
         db_connect1 = db_connect()
         connect1 = db_connect1.connect_to_db()
         cursor = connect1.cursor()
 
-        self.name=input("Please input your name: ")
-        self.email=input("Please input your email: ")
         while True:
-            self.password1 = input("Please input your account passoerd: ")
-            self.password = input("Please input your passowrd again: ")
-            if self.password1 == self.password:
-                print("your passwords match!\n")
-                break
+            self.name=input("Please enter your account user name: ").strip()
+            if not self.name:
+                print("The field is empty, please type a valid input")
+            elif not any (char.isalpha() for char in self.name):
+                print("Your assignmennt name should contain at least one alphabetic character, please try again")        
+            elif len(self.name) < 3 or len(self.name) > 50:
+                    print("Your name should be between 3 and 50 characters long, please try again")
             else:
-                print("your passwords don't match please try again")
+                    break
 
+        while True:
+            self.email=input("Please input your email: ")
+            if not self.email:
+                print("The field is empty, please type a valid input")
+            elif not any (char.isalpha() for char in self.email):
+                print("Your email name should contain at least one alphabetic character, please try again")        
+            elif "@" not in self.email or "." not in self.email :
+                print("Your email should contain an @ symbol and a dot.")
+            elif len(self.email) < 5 or len(self.email) > 50:
+                print("Your email should be between 5 and 50 characters long, please try again")
+            else:           
+                    break
+
+        while True:
+            self.password1 = input("Please input your account password: ")
+            if not self.password1:
+                print("The field is empty, please type a valid input")
+            elif not 4 <= len(self.password1) <= 15:
+                print("Your password should be between 4 and 15 characters long, please try again")
+            elif not any (char.isalpha() for char in self.password1):
+                print("Your password should contain at least one alphabetic character, please try again")        
+            elif not any (char.isdigit() for char in self.password1):
+                print("Your password should contain at least one numeric character, please try again")        
+            else:
+                self.password = input("Please input your passowrd again: ")
+                if self.password1 == self.password:
+                    print("your passwords match!")
+                    print(f"Thank you {self.name} for registering with Early Aid\n")
+                    break
+                else:
+                    print("your passwords don't match please try again")
+
+            
         cursor.execute("""
             INSERT INTO users_parent (name, email, password)
             VALUES (%s, %s,%s)
@@ -37,28 +73,67 @@ class User:
         cursor.close()
         connect1.close()
 
-        print("Please provide your child's details for registration")
 
     def Child_details(self):
         db_connect1 = db_connect()
         connect1 = db_connect1.connect_to_db()
         cursor = connect1.cursor()
 
-
-        children_no=int(input("How many children do you have under 12 years? "))
+        print("Please provide your child's details for registration")  
+        while True:
+            children_no=(input("How many children do you have under 12 years? "))
+            if not 0<= children_no < 10 :
+                print("Children number should be between 0 and 10, please try again")
+            elif not children_no:
+                print("The field is empty, please type a valid input")
+            elif not children_no.isdigit():
+                print("Your input should be a number, please try again")
+            else :
+                break
         for i in range (children_no):
-            self.child_name = input(f"Please please provide child no.{i+1}'s name:  ")
-            self.age = input("Please input their age: ")
-            self.prev_diagnosis = input(f"Does {self.child_name} have any previous diagnosis or underlying conditions? ")
-            
+            while True:
+                self.child_name = input(f"Please please provide child no.{i+1}'s name:  ")
+                if not self.child_name:
+                    print("The field is empty, please type a valid input")
+                elif not any (char.isalpha() for char in self.child_name):
+                    print("Your child's name should contain at least one alphabetic character, please try again")        
+                elif len(self.child_name) < 3 or len(self.child_name) > 50:
+                    print("Your child's name should be between 3 and 50 characters long, please try again")
+                else:
+                    break
+            while True:
+                self.age = (input(f"Please input {self.child_name}'s age: "))
+                if not self.age:
+                    print("The field is empty, please type a valid input")
+                elif not self.age.isdigit():
+                    print("Your child's age should be a number, please try again")
+                elif not 0 <= int(self.age) <= 12:
+                    print("Your child's age should be between 0 and 12 years, please try again")
+                else:
+                    break
+            #Should we have a list of common conditions for previous diagnosis?
+
+            while True:
+                self.prev_diagnosis = input(f"Does {self.child_name} have any previous diagnosis or underlying conditions? ")
+                if not self.prev_diagnosis:
+                    print("The field is empty, please type a valid input")
+                elif not any (char.isalpha() for char in self.prev_diagnosis):
+                    print("Your input should contain at least one alphabetic character, please try again")
+                elif len(self.prev_diagnosis)<4:
+                    print("Your input should be at least 4 characters long, please try again")
+                else:
+                    break   
+
+            # Fetching the user_id of the parent to link with the child
             cursor.execute("SELECT user_id FROM users_parent WHERE email = %s",(self.email,))
-            parent_id = cursor.fetchone()[0]
+            self.parent_id = cursor.fetchone()[0]
             connect1.commit()
 
+            # Inserting the child's details into the database
             cursor.execute("""
             INSERT INTO children (user_id,name, age, prev_diagnosis)
             VALUES (%s,%s,%s,%s)
-            """, (parent_id, self.child_name, self.age, self.prev_diagnosis))
+            """, (self.parent_id, self.child_name, self.age, self.prev_diagnosis))
            
             connect1.commit()
         
@@ -74,22 +149,43 @@ class User:
         cursor = connect1.cursor()
 
         print("Welcome back! Please login to your account")
-        self.email= input("Please input your account email: ")
+        while True:
+            self.email= input("Please input your account email: ")
+            if not self.email:
+                print("The field is empty, please type a valid input")
+            else:
+                break
         login_query = "SELECT password FROM users_parent WHERE email =%s"
         cursor.execute(login_query, (self.email,))
         output = cursor.fetchone()
         #try calling the reg login,ask yes onr no questions,while loop
         if output is None:
             print("This email is not registered, please register first before logging in or use as guest user")
+            choice = input ("would you like to register or login again? choose either 1 or 2:\n1.Register\n2.Login\n")
+            if choice == "1":
+                self.Reg_user()
+                self.Child_details()
+                self.Login()
+            elif choice == "2":
+                self.Login()
+            else:
+                print("Invalid input, please try again")
+                self.Login()
+            
         else:
+
             self.password1 = input("Please input your account password: ")
             if self.password1 == output[0]:
                 print("You have succesfully logged in. Welcome to Early Aid! ")
+            else:
+                print("Your password is incorrect, please try again")
+                self.Login()
+
         cursor.commit()
         
         
         child_query = "SELECT child_name FROM children WHERE user_id = %s"
-        cursor.execute(child_query, (18,))
+        cursor.execute(child_query, (output[0],))
         child_output = cursor.fetchall()
         for i, child in enumerate(child_output,start = 1):
             print(f"{i}.{child[0]}")
@@ -125,7 +221,7 @@ class User:
     
 
 user1=User()
-user1.user_acess()          
+user1.user_acess()         
     # def inputvalidation(self,input_type):
     #     input_type = input_type.strip()
 
