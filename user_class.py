e42cde7710c96025cd1a188d308d8fe5891e6ec0
G-Njunge <g.njunge@alustudent.com>
2025-07-23 14:13:59 +0200

    mod

--- user_class.py
+++ user_class.py
@@ -18,7 +18,7 @@ class User:
         cursor = connect1.cursor()
 
         self.name=input("Please input your name: ")
-        self.email=input("Please input your email ")
+        self.email=input("Please input your email: ")
         while True:
             self.password1 = input("Please input your account passoerd: ")
             self.password = input("Please input your passowrd again: ")
@@ -37,7 +37,6 @@ class User:
         cursor.close()
         connect1.close()
 
-        print(f"Dear {self.name} your account has been succesfully registered!\n")
         print("Please provide your child's details for registration")
 
     def Child_details(self):
@@ -54,7 +53,8 @@ class User:
             
             cursor.execute("SELECT user_id FROM users_parent WHERE email = %s",(self.email,))
             parent_id = cursor.fetchone()[0]
-            
+            connect1.commit()
+
             cursor.execute("""
             INSERT INTO children (user_id,name, age, prev_diagnosis)
             VALUES (%s,%s,%s,%s)
@@ -65,14 +65,16 @@ class User:
             print(f" Your child: {self.child_name} has been registered ")
         cursor.close()
         connect1.close()
-        print("All your children have been registered successfully!\n")
+        print("All your children have been registered, and your account has been created")
+
 
     def Login(self):
         db_connect1=db_connect()
         connect1 = db_connect1.connect_to_db()
         cursor = connect1.cursor()
 
-        self.email= input("Please input your email: ")
+        print("Welcome back! Please login to your account")
+        self.email= input("Please input your account email: ")
         login_query = "SELECT password FROM users_parent WHERE email =%s"
         cursor.execute(login_query, (self.email,))
         output = cursor.fetchone()
@@ -119,8 +121,6 @@ class User:
                 print("Accesing app as guest ")
                 self.Guest()
     
-   
-
 
 user1=User()
 user1.user_acess()          
