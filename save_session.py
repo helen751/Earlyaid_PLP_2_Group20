7842dab403bd34ffd6304480333384e7813a1c5c
bendoujanna <b.vitalina@alustudent.com>
2025-07-24 23:32:07 +0200

    Updated save_session.py

--- save_session.py
+++ save_session.py
@@ -1,33 +1,42 @@
-import sqlite3  # Module for SQLite database operations
 from datetime import datetime  # Generate a readable timestamp
+import mysql.connector
+from db_connect import db_connect
 
-def store_session_result(response_id, user_id, child_id, answer, suggestion, risk_score, timestamp):
+# Establish and reuse the connection
+connect = db_connect()
+connection = connect.connect_to_db()
+
+def store_session_result(response_id, user_id, child_id, answer, suggestion, risk_score, timestamp=None):
     """
     Store the user session results, the user id and the child id into the earlyaid database
     """
+
+    #If there is no timestamp provided, use the current date and time
+    if timestamp is None:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     try:
-        # Get the current date and time
-        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # Create a cursor that execute sql commands
+        cursor = connection.cursor()
 
-        # Connect to the database
-        conn = sqlite3.connect('earlyaid_database_db.db')
-        cursor = conn.cursor()
-
-        # Insert the session result into the 'responses' table
-        cursor.execute('''
+        sql = '''
         INSERT INTO responses (response_id, user_id, child_id, answer, suggestion, risk_score, timestamp)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''', (response_id, user_id, child_id, answer, suggestion, risk_score, timestamp))
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        '''
+
+        # Execute sql command with the values provided
+        cursor.execute(sql, (response_id, user_id, child_id, answer, suggestion, risk_score, timestamp))
+
 
         # Save the changes into the database
-        conn.commit()
+        connection.commit()
 
         print("The information has been saved successfully!")
 
-    except sqlite3.Error as e:  # Print any database error that occurs
+    except mysql.connector.Error as e:  # Print any database error that occurs
         print("Error saving the session to the database: ", e)
 
     finally:
         # Close the connection if it was opened
-        if 'conn' in locals():
-            conn.close()
+        if 'connection' in locals():
+            connection.close()
